7d0dbf247d92a1114e44d8262e0fea6913ea8313
Lucas <lucas.tiziani@gatech.edu>
2021-01-03 15:18:01 -0500

    no change

--- msp-ws/balance-bot/uart_cust.c
+++ msp-ws/balance-bot/uart_cust.c
@@ -13,7 +13,7 @@
 #include "uart_cust.h"
 
 
-void UARTc_sendFloatArray(float* arr, int len_arr) {
+void UARTc_sendFloatArray(float * arr, int len_arr) {
     /* transmit float data array over UART */
     int i,j;
     char str[20];
@@ -21,16 +21,24 @@ void UARTc_sendFloatArray(float* arr, int len_arr) {
         sprintf(str, "%12.3f", arr[i]); // convert data float to string
         j = 0;
         while (str[j] != 0x00) {
-            MAP_UART_transmitData(EUSCI_A0_BASE, str[j]); // transmit digit in data string
-            while (MAP_UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) != EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG); // wait for transmit buffer empty
+            MAP_UART_transmitData(EUSCI_A0_BASE, str[j]); // transmit char
+            while (MAP_UART_getInterruptStatus(EUSCI_A0_BASE,
+                EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
+                != EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG); // wait for transmit buffer empty
             j++;
         }
         MAP_UART_transmitData(EUSCI_A0_BASE, 0x20); // transmit space between vector values
-        while (MAP_UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) != EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG); // wait for transmit buffer empty
+        while (MAP_UART_getInterruptStatus(EUSCI_A0_BASE,
+            EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
+            != EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG); // wait for transmit buffer empty
     }
     MAP_UART_transmitData(EUSCI_A0_BASE, 0x0D); // transmit carriage return between data sets
-    while (MAP_UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) != EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG); // wait for transmit buffer empty
+    while (MAP_UART_getInterruptStatus(EUSCI_A0_BASE,
+        EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
+        != EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG); // wait for transmit buffer empty
     MAP_UART_transmitData(EUSCI_A0_BASE, 0x0A); // transmit new line between data sets
-    while (MAP_UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) != EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG); // wait for transmit buffer empty
+    while (MAP_UART_getInterruptStatus(EUSCI_A0_BASE,
+        EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
+        != EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG); // wait for transmit buffer empty
 }
 
